6dc2437d4d78443cc0f285b802e7b7807ecef8f3
dacup72 <dacup72@gmail.com>
2018-10-09 16:08:56 -0600

    added ToDo item model

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const mongoKey = "mongodb://dacup72:password1@ds127293.mlab.com:27293/tododb";
 
 mongoose.connect(mongoKey, { useNewUrlParser: true }, function (error) {
   if (error) {
     return console.log("the connection broke");
-  } else {
+  } 
+  else {
     console.log("mongoose connection successful");
   }
 });
 
-module.exports = mongoose;
+export default = mongoose;
