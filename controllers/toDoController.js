84642aabc183b23576422a5143af08a682a39390
dacup72 <dacup72@gmail.com>
2018-10-10 13:04:18 -0600

    origional request fully functioning now

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,28 +1,28 @@
 const db = require("../models");
 
 module.exports = {
-  findAll: (req, res) => (
+  findAll: (req, res) => {
     db.TodoItems
-      .findAll()
+      .find({})
       .then(todos => res.json(todos))
       .catch((err) => res.status(500).json(err))
-  ),
-  createItem: (req, res) => (
+  },
+  createItem: (req, res) => {
     db.TodoItems
       .create(req.body)
       .then(todo => res.json(todo))
       .catch(err => res.status(500).json(err))
-  ),
-  deleteItem: (req, res) => (
+  },
+  deleteItem: (req, res) => {
     db.TodoItems
-      .delete(req.params.id)
+      .deleteOne({_id: req.params.id})
       .then(() => res.sendStatus(200))
       .catch(err => res.status(500).json(err))
-  ),
-  updateItem: (req, res) => (
+  },
+  updateItem: (req, res) => {
     db.TodoItems
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(todo => res.json(todo))
       .catch(err => res.sendStatus(501).json(err))
-  )
+  }
 }
